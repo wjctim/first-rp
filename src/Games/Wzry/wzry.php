e8c8ba354a9e8e14d740ce653abc7b468c90b9ad
tim <wang.jia.cheng@foocaa.com>
2017-02-04 10:32:04 +0800

    add files

---
+++ src/Games/Wzry/wzry.php
@@ -0,0 +1,11 @@
+<?php
+ 
+namespace Games;
+ 
+class Wzry
+{
+    public static function info()
+    {
+        echo "This is Wzry!<br />";
+    }
+}
---
+++ src/Games/wzry.php
@@ -0,0 +1,11 @@
+<?php
+ 
+namespace Games\Wzry;
+ 
+class Wzry2017
+{
+    public static function info()
+    {
+        echo "This is Wzry!<br />";
+    }
+}
